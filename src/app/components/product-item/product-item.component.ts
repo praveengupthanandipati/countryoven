bd194d5523e4eda60d4849d8d9bb6af0ef5b734a
Praveen Guptha <56840552+praveengupthanandipati@users.noreply.github.com>
2023-08-20 23:00:51 +0530

    commit home page

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-item.component.scss']
 })
 export class ProductItemComponent {
-
+  @Input() Productvalue:any;
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,100 @@ import { Component } from '@angular/core';
   styleUrls:['./home.component.scss']
 })
 export class HomeComponent {
- 
+  productvalues:any[] = [
+    {
+    Image: 'assets/images/products/product01.jpg', 
+    Sellerlabel:'Best Seller', 
+    Title:'Lovely Black Forest Cake with Red Roses with 2 kg',
+    OfferPrice:'1299.00',
+    Mrp:'1500.00',
+    Offer:'(17% Off)',
+    Rating:'4.7',
+    DeliveryEstimate:'Today'
+    },
+    {
+    Image: 'assets/images/products/product02.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Lovely Black Forest Cake with Red Roses with 3 kg',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Tomorrow'
+    },
+    {
+    Image: 'assets/images/products/product03.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Black Forest Cake with Red Roses with 1 kg',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Tomorrow'
+    },
+    {
+    Image: 'assets/images/products/product04.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Lovely Black Forest Cake with Red Roses with 3 kg Forest Cake with',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Today'
+    },   
+  ];
+  
+  birthdaycollectionsitems:any[] = [
+    {
+      Image:'assets/images/collections/collection01.jpg',
+      Name:'Cakes'
+    },
+    {
+      Image:'assets/images/collections/collection02.jpg',
+      Name:'Combos'
+    },
+    {
+      Image:'assets/images/collections/collection03.jpg',
+      Name:'Flowers'
+    },
+    {
+      Image:'assets/images/collections/collection04.jpg',
+      Name:'Plants'
+    },
+    {
+      Image:'assets/images/collections/collection05.jpg',
+      Name:'Personalised Gifts'
+    },
+    {
+      Image:'assets/images/collections/collection06.jpg',
+      Name:'Personalised Chocolates'
+    },
+  ];
+  
+  anniversarycollectionitems:any[] = [
+    {
+      Image:'assets/images/collections/collection07.jpg',
+      Name:'Flowers'
+    },
+    {
+      Image:'assets/images/collections/collection08.jpg',
+      Name:'Combos'
+    },
+    {
+      Image:'assets/images/collections/collection09.jpg',
+      Name:'Cakes'
+    },
+    {
+      Image:'assets/images/collections/collection10.jpg',
+      Name:'Plants'
+    },
+    {
+      Image:'assets/images/collections/collection11.jpg',
+      Name:'Personalised Gifts'
+    },
+    {
+      Image:'assets/images/collections/collection12.jpg',
+      Name:'Personalised Flowers'
+    },
+  ]
 }
