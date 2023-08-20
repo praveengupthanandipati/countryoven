864d0b01734c8568517d1ca6215c102e04d4be0d
Praveen Guptha <56840552+praveengupthanandipati@users.noreply.github.com>
2023-08-21 01:02:35 +0530

    product list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'products-list', component:ProductListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -6,5 +6,184 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {
+  Filteritems=[
+    {subcatname:'New Collection'},
+    {subcatname:'Best Sellers'},
+    {subcatname:'Fruit Cakes'},
+    {subcatname:'Fancy Cakes'},
+    {subcatname:'Tier Cakes'},
+    {subcatname:'Best Collection'},
+    {subcatname:'Designer Cakes'},
+    {subcatname:'Kids Cakes'},
+    {subcatname:'Emoji Cakes'},
+    {subcatname:'Chocolate Cakes'},
+    {subcatname:'Barbie Cakes'},
+    {subcatname:'Theme Cakes'},
+    {subcatname:'Photo Cakes'},
+    {subcatname:'Customized Cakes'},
+    {subcatname:'Theme Cakes'},
+  ];
 
+  productvalues:any[] = [
+    {
+    Image: 'assets/images/products/product01.jpg', 
+    Sellerlabel:'Best Seller', 
+    Title:'Lovely Black Forest Cake with Red Roses with 2 kg',
+    OfferPrice:'1299.00',
+    Mrp:'1500.00',
+    Offer:'(17% Off)',
+    Rating:'4.7',
+    DeliveryEstimate:'Today'
+    },
+    {
+    Image: 'assets/images/products/product02.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Lovely Black Forest Cake with Red Roses with 3 kg',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Tomorrow'
+    },
+    {
+    Image: 'assets/images/products/product03.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Black Forest Cake with Red Roses with 1 kg',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Tomorrow'
+    },
+    {
+    Image: 'assets/images/products/product04.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Lovely Black Forest Cake with Red Roses with 3 kg Forest Cake with',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Today'
+    },  
+    {
+    Image: 'assets/images/products/product05.jpg', 
+    Sellerlabel:'Best Seller', 
+    Title:'Lovely Black Forest Cake with Red Roses with 2 kg',
+    OfferPrice:'1299.00',
+    Mrp:'1500.00',
+    Offer:'(17% Off)',
+    Rating:'4.7',
+    DeliveryEstimate:'Today'
+    },
+    {
+    Image: 'assets/images/products/product06.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Lovely Black Forest Cake with Red Roses with 3 kg',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Tomorrow'
+    },
+    {
+    Image: 'assets/images/products/product07.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Black Forest Cake with Red Roses with 1 kg',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Tomorrow'
+    },
+    {
+    Image: 'assets/images/products/product08.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Lovely Black Forest Cake with Red Roses with 3 kg Forest Cake with',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Today'
+    }, 
+    {
+      Image: 'assets/images/products/product09.jpg', 
+      Sellerlabel:'Best Seller', 
+      Title:'Lovely Black Forest Cake with Red Roses with 2 kg',
+      OfferPrice:'1299.00',
+      Mrp:'1500.00',
+      Offer:'(17% Off)',
+      Rating:'4.7',
+      DeliveryEstimate:'Today'
+      },
+      {
+      Image: 'assets/images/products/product10.jpg', 
+      Sellerlabel:'New Launch', 
+      Title:'Lovely Black Forest Cake with Red Roses with 3 kg',
+      OfferPrice:'1359.00',
+      Mrp:'1800.00',
+      Offer:'(12% Off)',
+      Rating:'5.0',
+      DeliveryEstimate:'Tomorrow'
+      },
+      {
+      Image: 'assets/images/products/product11.jpg', 
+      Sellerlabel:'New Launch', 
+      Title:'Black Forest Cake with Red Roses with 1 kg',
+      OfferPrice:'1359.00',
+      Mrp:'1800.00',
+      Offer:'(12% Off)',
+      Rating:'5.0',
+      DeliveryEstimate:'Tomorrow'
+      },
+      {
+      Image: 'assets/images/products/product12.jpg', 
+      Sellerlabel:'New Launch', 
+      Title:'Lovely Black Forest Cake with Red Roses with 3 kg Forest Cake with',
+      OfferPrice:'1359.00',
+      Mrp:'1800.00',
+      Offer:'(12% Off)',
+      Rating:'5.0',
+      DeliveryEstimate:'Today'
+      },
+      {
+        Image: 'assets/images/products/product13.jpg', 
+        Sellerlabel:'Best Seller', 
+        Title:'Lovely Black Forest Cake with Red Roses with 2 kg',
+        OfferPrice:'1299.00',
+        Mrp:'1500.00',
+        Offer:'(17% Off)',
+        Rating:'4.7',
+        DeliveryEstimate:'Today'
+        },
+        {
+        Image: 'assets/images/products/product14.jpg', 
+        Sellerlabel:'New Launch', 
+        Title:'Lovely Black Forest Cake with Red Roses with 3 kg',
+        OfferPrice:'1359.00',
+        Mrp:'1800.00',
+        Offer:'(12% Off)',
+        Rating:'5.0',
+        DeliveryEstimate:'Tomorrow'
+        },
+        {
+        Image: 'assets/images/products/product15.jpg', 
+        Sellerlabel:'New Launch', 
+        Title:'Black Forest Cake with Red Roses with 1 kg',
+        OfferPrice:'1359.00',
+        Mrp:'1800.00',
+        Offer:'(12% Off)',
+        Rating:'5.0',
+        DeliveryEstimate:'Tomorrow'
+        },
+        {
+        Image: 'assets/images/products/product16.jpg', 
+        Sellerlabel:'New Launch', 
+        Title:'Lovely Black Forest Cake with Red Roses with 3 kg Forest Cake with',
+        OfferPrice:'1359.00',
+        Mrp:'1800.00',
+        Offer:'(12% Off)',
+        Rating:'5.0',
+        DeliveryEstimate:'Today'
+        },
+  ];
 }
