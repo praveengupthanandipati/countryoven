e3b1236b1ba76ebebc12bbad67788ea3c04963a4
Praveen Guptha <56840552+praveengupthanandipati@users.noreply.github.com>
2023-08-26 22:52:53 +0530

    commit product detail

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -9,4 +9,47 @@ export class ProductDetailComponent {
   
   productname='Chocolate Truffle Delicious Cake Half Kg';
 
+  productvalues:any[] = [
+    {
+    Image: 'assets/images/products/product01.jpg', 
+    Sellerlabel:'Best Seller', 
+    Title:'Lovely Black Forest Cake with Red Roses with 2 kg',
+    OfferPrice:'1299.00',
+    Mrp:'1500.00',
+    Offer:'(17% Off)',
+    Rating:'4.7',
+    DeliveryEstimate:'Today'
+    },
+    {
+    Image: 'assets/images/products/product02.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Lovely Black Forest Cake with Red Roses with 3 kg',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Tomorrow'
+    },
+    {
+    Image: 'assets/images/products/product03.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Black Forest Cake with Red Roses with 1 kg',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Tomorrow'
+    },
+    {
+    Image: 'assets/images/products/product04.jpg', 
+    Sellerlabel:'New Launch', 
+    Title:'Lovely Black Forest Cake with Red Roses with 3 kg Forest Cake with',
+    OfferPrice:'1359.00',
+    Mrp:'1800.00',
+    Offer:'(12% Off)',
+    Rating:'5.0',
+    DeliveryEstimate:'Today'
+    },   
+  ];
+
 }
