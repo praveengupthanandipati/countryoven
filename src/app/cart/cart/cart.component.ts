7a5792ae9e004bebdb45a5c99af5b6e0a72c1ba9
Praveen Guptha <56840552+praveengupthanandipati@users.noreply.github.com>
2023-09-03 12:54:32 +0530

    commit files

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+  cartCount:number =4;
 
+  constructor(){
+    function increaseValue():void {
+      const inputElement = document.getElementById('number') as HTMLInputElement;
+      let value: number = parseInt(inputElement.value, 10);
+      value = isNaN(value) ? 0 : value;
+      value++;
+      inputElement.value = value.toString();
+    }
+    function decreaseValue():void {
+      const inputElement = document.getElementById('number') as HTMLInputElement;
+      let value: number = parseInt(inputElement.value, 10);
+      value = isNaN(value) ? 0 : value;
+      value = value < 1 ? 1 : value;
+      value--;
+      inputElement.value = value.toString();
+    }
+  }  
+
+  CartItems:any=[
+    {
+      Image: 'assets/images/products/product01.jpg', 
+      Name: 'Precious Beauty',
+      Offerprice:1299,
+      Mrp:1500,
+      Percentage:17,
+      ShippingType:'Express'
+    },
+    {
+      Image: 'assets/images/products/product02.jpg', 
+      Name: 'Precious Beauty',
+      Offerprice:1299,
+      Mrp:1500,
+      Percentage:17,
+      ShippingType:'Express'
+    },
+    {
+      Image: 'assets/images/products/product03.jpg', 
+      Name: 'Precious Beauty',
+      Offerprice:1299,
+      Mrp:1500,
+      Percentage:17,
+      ShippingType:'Express'
+    }
+  ];
+ 
 }
