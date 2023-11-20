1410199160eb4cd0eff3624db8a847e8ca24f415
Praveen Guptha <56840552+praveengupthanandipati@users.noreply.github.com>
2023-11-20 08:00:01 +0530

    static pages created

---
+++ src/app/pages/customized-cakes/customized-cakes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customized-cakes',
+  templateUrl: './customized-cakes.component.html',
+  styleUrls: ['./customized-cakes.component.scss']
+})
+export class CustomizedCakesComponent {
+
+}
---
+++ src/app/pages/deliver-cities/deliver-cities.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deliver-cities',
+  templateUrl: './deliver-cities.component.html',
+  styleUrls: ['./deliver-cities.component.scss']
+})
+export class DeliverCitiesComponent {
+
+}
