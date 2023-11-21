e375332b999ff0b9c3c7585f341330e52f40e12c
Praveen Guptha <56840552+praveengupthanandipati@users.noreply.github.com>
2023-11-21 07:59:12 +0530

    static pages

---
+++ src/app/pages/returnpolicy/returnpolicy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-returnpolicy',
+  templateUrl: './returnpolicy.component.html',
+  styleUrls: ['./returnpolicy.component.scss']
+})
+export class ReturnpolicyComponent {
+
+}
