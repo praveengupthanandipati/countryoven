797124aaccfbd5057ad70f0fa10230d00896c8d2
Krishna <krishna@myverkoper.com>
2023-11-28 10:51:02 +0530

    changes

--- src/app/components/header/midheader/midheader.component.ts
+++ src/app/components/header/midheader/midheader.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Subscription } from 'rxjs';
 import { CurdService } from 'src/app/services/curd.service';
 
@@ -14,8 +15,13 @@ export class MidheaderComponent  implements OnInit{
   custID:any;
   custName:any;
   count:any;
+  sessionId:any;
+  city:any;
+  countryname:any;
+  currency:any;
+  customerId:any=0;
   private subscription!: Subscription;
-  constructor(private _curdService:CurdService)
+  constructor(private _curdService:CurdService,private cookieService: CookieService)
   {
    if(localStorage.getItem('email'))
    {
@@ -26,14 +32,47 @@ export class MidheaderComponent  implements OnInit{
 
 
 
+   this.sessionId= this.cookieService.get('sessionID')
+   this.city=localStorage.getItem('city')
+this.countryname=localStorage.getItem('country');
+this.currency=localStorage.getItem('currency');
+if(localStorage.getItem('customerId'))
+{
+ this.customerId=localStorage.getItem('customerId')
+}
+
   }
   ngOnInit(): void {
       this.subscription = this._curdService.headerData$.subscribe((data) => {
       console.log(data)
       this.count=data;
     });
+    this.getCarts();
   }
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
+
+
+
+
+  getCarts()
+  {
+  let data={
+      customerId: this.customerId,
+      sessionId: this.sessionId,
+      cityName: this.city,
+      countryName: this.countryname,
+      currencySelected: this.currency
+    }
+  
+    this._curdService.postShopingCart(data).subscribe(res => {
+      console.log(res)
+     
+      this.count=res.length;
+  
+          });
+  }
+
+
 }
